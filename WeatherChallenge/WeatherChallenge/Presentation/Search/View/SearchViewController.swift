e0072b5b9573efd06ed392ac74cf675c620b44d4
Binshakerr <binshakerr@gmail.com>
2021-12-17 10:55:33 +0200

    project start

---
+++ WeatherChallenge/WeatherChallenge/Presentation/Search/View/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  WeatherChallenge
+//
+//  Created by Eslam Shaker on 17/12/2021.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = "Search"
+    }
+
+}
---
+++ WeatherChallenge/WeatherChallenge/Presentation/Search/ViewModel/SearchViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SearchViewModel.swift
+//  WeatherChallenge
+//
+//  Created by Eslam Shaker on 17/12/2021.
+//
+
+import Foundation
+
+class SearchViewModel {
+    
+}
